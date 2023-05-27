89c6dd2f209a8a9e6ac7186e22662a99c15771c6
bbangjun <lucid9dawn@gmail.com>
2023-05-27 17:31:00 +0900

    feat(PictureBoardRepository): JPA repository 생성

---
+++ src/main/java/com/example/Proj1_2019202023/repository/PictureBoardRepository.java
@@ -0,0 +1,7 @@
+package com.example.Proj1_2019202023.repository;
+
+import com.example.Proj1_2019202023.entity.PictureBoardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PictureBoardRepository extends JpaRepository<PictureBoardEntity, Long> {
+}
